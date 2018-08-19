803d0dc20c8d278f2d88516f6c5ef909c26fa9d9
Andrea <andry.tino@gmail.com>
2018-08-19 21:51:43 +0200

    More stuff

---
+++ src/cgal-qt-interop/Runner/Runner/main.cpp
@@ -0,0 +1,15 @@
+/*
+Andrea Tino - 2018
+*/
+
+#include <iostream>
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/IO/Qt_widget.h>
+
+int main(int, char**);
+
+int main(int argc, char** argv) 
+{
+	return 0;
+}
